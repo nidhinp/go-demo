f7b3fc548da2eee260b8d2a8d1a3c2cf5ae45ed5
nidhinp <nidhinp.412@gmail.com>
2020-05-13 16:18:25 +1200

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	r := http.NewServeMux()
+	r.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello From Go!")
+	})
+	s := &http.Server{
+		Addr:        ":8080",
+		Handler:     r,
+		ReadTimeout: 10 * time.Second,
+	}
+	fmt.Println("Starting server on port 8080")
+	log.Fatal(s.ListenAndServe())
+}
